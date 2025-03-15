ce25a927ed0d4eefa2b1b8cdbad17295d74dfb98
Muhammadsolih Abdugafforov <salikhdev@gmail.com>
2025-03-15 19:31:24 +0500

    Skill added 1.1v

--- src/main/java/school/faang/user_service/service/SkillService.java
+++ src/main/java/school/faang/user_service/service/SkillService.java
@@ -1,9 +1,11 @@
 package school.faang.user_service.service;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.repository.SkillRepository;
 
 @Service
+@Repository
 public class SkillService {
 
     private SkillRepository skillRepository;
