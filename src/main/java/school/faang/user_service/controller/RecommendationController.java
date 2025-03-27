0796093b753b68a1ae9ef72531d5d280881eaf73
Suhrob <suhrob.a001@gmail.com>
2025-03-27 17:04:56 +0500

    dtos and a empty service file is added

--- src/main/java/school/faang/user_service/controller/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/RecommendationController.java
@@ -13,6 +13,6 @@ public class RecommendationController {
 
     @PostMapping("/create")
     public ResponseEntity<?> giveRecommendation(@RequestBody RecommendationDto recommendationDto){
-
+        return ResponseEntity.ok("something");
     }
 }
